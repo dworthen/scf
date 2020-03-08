7f654de39a116ca8820f4a7314865453be1c0621
Derek Worthen <worthend.derek@gmail.com>
2020-03-08 10:31:54 -0700

    Update process to async

--- projects/cli/src/index.ts
+++ projects/cli/src/index.ts
@@ -1,18 +1,25 @@
 import minimist from "minimist";
-import { minimistOptions } from "./Config";
+import { minimistOptions, loadSkafConfig } from "./Config";
 import { displayUsageInfo, printVersion } from "./bin";
 import { exists } from "fs";
+import { runInContext } from "vm";
 
-let argv = minimist(process.argv.slice(2), minimistOptions);
+async function run(): Promise<void> {
+  let argv = minimist(process.argv.slice(2), minimistOptions);
 
-if (argv.help) {
-  console.log("");
-  printVersion();
-  displayUsageInfo(argv);
-  process.exit();
+  if (argv.help) {
+    console.log("");
+    printVersion();
+    displayUsageInfo(argv);
+    process.exit();
+  }
+
+  if (argv.version) {
+    printVersion();
+    process.exit();
+  }
 }
 
-if (argv.version) {
-  printVersion();
+run().then(_ => {
   process.exit();
-}
+});
