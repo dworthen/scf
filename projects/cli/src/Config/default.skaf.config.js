68047cf8ce247cf4c420671975866a45a50bd77b
Derek Worthen <worthend.derek@gmail.com>
2020-03-20 15:31:19 -0700

    Update default config settings

--- projects/cli/src/Config/default.skaf.config.js
+++ projects/cli/src/Config/default.skaf.config.js
@@ -1,6 +1,9 @@
 module.exports = argv => {
   return {
     localTemplateDirectory: ".skaf",
-    ignore: ["node_modules", ".git", "nua.config.js"]
+    ignore: ["node_modules", ".git", "skaf.config.js"],
+    formatter: async function(fileObj) {
+      return fileObj.contents;
+    }
   };
 };
