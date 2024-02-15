765b1254b795b5cbebd1df54947a39274474cf58
Derek Worthen <worthend.derek@gmail.com>
2024-02-14 17:57:42 -0800

    testing js install

--- js/bin.ts
+++ js/bin.ts
@@ -7,6 +7,7 @@ function run() {
 	console.log(__dirname);
 	const stdout = execSync("npm root -g", { encoding: "utf-8" });
 	console.log(stdout);
+	console.log(process.env["npm_config_local_prefix"]);
 	console.log(process.env["npm_config_prefix"]);
 }
 
