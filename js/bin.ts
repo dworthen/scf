c014a80ff25d09dd4d79bdcb6449e674dedd92ff
Derek Worthen <worthend.derek@gmail.com>
2024-02-14 17:55:13 -0800

    testing js install

--- js/bin.ts
+++ js/bin.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-
+import { execSync } from "node:child_process";
 import { runScf } from "./index.js";
 
-async function run() {
+function run() {
 	console.log(__filename);
 	console.log(__dirname);
+	const stdout = execSync("npm root -g", { encoding: "utf-8" });
+	console.log(stdout);
+	console.log(process.env["npm_config_prefix"]);
 }
 
 void run();
